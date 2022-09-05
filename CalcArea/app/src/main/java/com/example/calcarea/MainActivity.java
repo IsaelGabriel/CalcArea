0cc55e41c8a063b9f53519e14c678d92284d169f
isa_msconfig_el <75548498+IsaelGabriel@users.noreply.github.com>
2022-09-05 14:33:16 -0300

    Add files via upload

--- CalcArea/app/src/main/java/com/example/calcarea/MainActivity.java
+++ CalcArea/app/src/main/java/com/example/calcarea/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.calcarea;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calc();
+
+    }
+    public void calc()
+    {
+        final Button button = (Button) findViewById(R.id.b_calc);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                int height = Integer.parseInt(((EditText) findViewById(R.id.e_height)).getText().toString());
+                int width = Integer.parseInt(((EditText) findViewById(R.id.e_width)).getText().toString());
+                TextView area = (TextView) findViewById(R.id.area_text);
+                area.setText("Área = " + Integer.toString(height*width));
+
+            }
+        });
     }
 }
